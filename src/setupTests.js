e50d614d8f716b49a6b8b242edc2ed8768f1d9ca
Philippe Ngom <philngom@gmail.com>
2022-04-29 14:48:47 -0700

    added search functionality. created fixtures file and added api data for msw

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,9 @@
 global.fetch = (...args) =>
   import('cross-fetch').then(({ default: fetch }) => fetch(...args));
+
+  import { rest } from 'msw';
+  import { setupServer } from 'msw/node';
+
+  import { charactersData } from './tests/fixtures/characterData';
+
+
