09f00d0545ff9141edf919f3dbffccece3836e3a
Philippe Ngom <philngom@gmail.com>
2022-04-29 14:09:24 -0700

    added noresults searching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Characters from './views/Characters';
 
 export default function App() {
   return (
-    <h1>The Office Characters</h1>
-    <Characters />
+    <>
+      <Characters />
+    </>
   )
 }
