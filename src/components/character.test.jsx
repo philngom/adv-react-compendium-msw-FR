f87efdf3ed310635e507ccf7bb7f4a1d19d6199c
Philippe Ngom <philngom@gmail.com>
2022-04-29 17:10:34 -0700

    finished component test

--- src/components/character.test.jsx
+++ src/components/character.test.jsx
@@ -1,18 +1,16 @@
-import App from '../App';
+import Characters from '../views/Characters';
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('App', () => {
+describe('Component test', () => {
   it('should load and display a list of 27 characters', async () => {
     render(
       <MemoryRouter>
-        <App />
+        <Characters />
       </MemoryRouter>
     )
-
-    screen.getByText(/loading.../i);
-
-    await screen.findByText(/michael scott/i);
+    const items = await screen.findAllByRole('listitem');
+    expect(items).toHaveLength(27);
   })
 })
